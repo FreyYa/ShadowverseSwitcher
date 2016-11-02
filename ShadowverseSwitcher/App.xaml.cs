131022f9a46e2ccef437dc5e05066bf7947fcb31
FreyYa <hong09072@gmail.com>
2016-11-02 16:33:39 +0900

    기초 UI 및 구조 구축

--- ShadowverseSwitcher/App.xaml.cs
+++ ShadowverseSwitcher/App.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Livet;
+using ShadowverseSwitcher.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +17,53 @@ namespace ShadowverseSwitcher
 	/// </summary>
 	public partial class App : Application
 	{
+		static string MainFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+		public static MainWindowViewModel ViewModelRoot { get; private set; }
+
+		static App()
+		{
+			AppDomain.CurrentDomain.UnhandledException += (sender, args) => ReportException(sender, args.ExceptionObject as Exception);
+		}
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			this.DispatcherUnhandledException += (sender, args) =>
+			{
+				ReportException(sender, args.Exception);
+				args.Handled = true;
+			};
+
+			DispatcherHelper.UIDispatcher = this.Dispatcher;
+
+			ViewModelRoot = new MainWindowViewModel();
+			this.MainWindow = new MainWindow { DataContext = ViewModelRoot };
+			this.MainWindow.Show();
+		}
+		#region 리포트
+		private static void ReportException(object sender, Exception exception)
+		{
+			#region const
+			const string messageFormat = @"
+===========================================================
+ERROR, date = {0}, sender = {1},
+{2}
+";
+			string path = Path.Combine(MainFolder, "error.log");
+			#endregion
+
+			try
+			{
+				var message = string.Format(messageFormat, DateTimeOffset.Now, sender, exception);
+
+				Debug.WriteLine(message);
+				File.AppendAllText(path, message);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+		}
+		#endregion
 	}
 }
---
+++ ShadowverseSwitcher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Livet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetroTrilithon.Mvvm;
+
+namespace ShadowverseSwitcher.ViewModels
+{
+	public class MainWindowViewModel : WindowViewModel
+	{
+		public MainWindowViewModel()
+		{
+			this.Title = "Shadowverse Switcher Beta";
+		}
+	}
+}
