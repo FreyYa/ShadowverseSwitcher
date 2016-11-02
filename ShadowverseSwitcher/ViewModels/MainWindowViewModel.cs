cf4e9a05c786f5137157cbc2b5a80c882085011e
FreyYa <hong09072@gmail.com>
2016-11-02 18:28:35 +0900

    설정 초기화 버튼 추가 및 라이브러리 갱신

--- ShadowverseSwitcher/ViewModels/MainWindowViewModel.cs
+++ ShadowverseSwitcher/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MetroTrilithon.Mvvm;
+using Microsoft.Win32;
 
 namespace ShadowverseSwitcher.ViewModels
 {
@@ -13,6 +14,13 @@ namespace ShadowverseSwitcher.ViewModels
 		public MainWindowViewModel()
 		{
 			this.Title = "Shadowverse Switcher Beta";
+		}
+		public void InitSetting()
+		{
+			if (Registry.CurrentUser.OpenSubKey(@"Software\Cygames\Shadowverse", false) != null)
+			{
+				Registry.CurrentUser.DeleteSubKeyTree(@"Software\Cygames\Shadowverse");
+			}
 		}
 	}
 }
